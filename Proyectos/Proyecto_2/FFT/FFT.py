73a96f746e141ae3daa2628dc9cca3390160860c
Giovanni <giovannilopez9808@gmail.com>
2021-03-22 01:52:30 -0600

    Change to ipynb to py

--- Proyectos/Proyecto_2/FFT/FFT.py
+++ Proyectos/Proyecto_2/FFT/FFT.py
@@ -3,48 +3,68 @@ from PIL import Image
 import numpy as np
 
 
-def plot_FFT_and_original(img, FFT, name, path=""):
-    fig = plt.figure(figsize=(8, 8))
-    axs = [fig.add_subplot(2, 2, i+1) for i in range(4)]
-    ax1, ax2, ax3, ax4 = axs
-    plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
-    plot_FFT(ax1, img, bar=False)
-    plot_FFT(ax2, FFT, cmap="binary")
-    plot_FFT(ax3, FFT, hexagon=True, cmap="binary")
-    plot_FFT(ax4, FFT, hexagon=True)
-    plt.savefig(path+name+".png", bbox_inches="tight",
-                pad_inches=0)
+class FFT_algorithm:
+    def __init__(self, path_image, name_image):
+        self.path_image = path_image
+        self.name_image = name_image
+        self.read_image()
+        self.apply_FFT()
+
+    def read_image(self):
+        self.img = Image.open(self.path_image + self.name_image)
+
+    def apply_FFT(self):
+        f = np.fft.fft2(self.img)
+        f = np.fft.fftshift(f)
+        self.FFT_image = np.log(np.abs(f)) * 1000
+
+    def plot_FFT_and_original(self):
+        fig = plt.figure(figsize=(6, 8))
+        axs = [fig.add_subplot(2, 2, i + 1) for i in range(4)]
+        ax1, ax2, ax3, ax4 = axs
+        plt.subplots_adjust(left=0,
+                            bottom=0,
+                            right=1,
+                            top=1,
+                            wspace=0,
+                            hspace=0)
+        self.plot_FFT(ax1, self.img, bar=False)
+        self.plot_FFT(ax2, self.FFT_image)
+        self.plot_FFT(ax3, self.FFT_image, hexagon=True)
+        self.plot_FFT(ax4, self.FFT_image, hexagon=True, cmap="inferno_r")
+        plt.subplots_adjust(top=0.978,
+                            bottom=0.022,
+                            left=0.025,
+                            right=0.975,
+                            hspace=0.0,
+                            wspace=0.054)
+        plt.show()
+
+    def plot_FFT(self, ax, image, cmap="Greys_r", bar=True, hexagon=False):
+        ax.axis("off")
+        ax.set_aspect('equal')
+        ax.imshow(image, cmap=cmap)
+        if bar:
+            ax.plot([6.5, 63.3], [247.4, 247.4], color="black", lw=4)
+            ax.text(2, 238, "5  1nm$^{-1}$", fontsize=14)
+        if hexagon:
+            self.read_plot_hexagon(ax)
+
+    def read_plot_hexagon(self, ax):
+        x, y = np.loadtxt("Data/hexa.csv", delimiter=",", unpack=True)
+        n = np.size(x)
+        for i in range(n):
+            j = i + 1
+            if j == n:
+                j = 0
+            pos = [i, j]
+            ax.plot(x[pos], y[pos], lw=3, color="black")
 
 
-def plot_FFT(ax, image, cmap=None, bar=True, hexagon=False):
-    ax.axis("off")
-    ax.set_aspect('equal')
-    ax.imshow(image, cmap=cmap)
-    if bar:
-        ax.plot([6.5, 63.3], [247.4, 247.4], color="black", lw=4)
-        ax.text(2, 238, "5  1nm$^{-1}$", fontsize=14)
-    if hexagon:
-        plot_hexagon(ax)
-
-
-def FFT(im):
-    f = np.fft.fft2(im)
-    f = np.fft.fftshift(f)
-    f = np.log(np.abs(f))*1000
-    return f
-
-
-def plot_hexagon(ax):
-    x, y = np.loadtxt("Data/hexa.csv", delimiter=",", unpack=True)
-    n = np.size(x)
-    for i in range(n):
-        j = i+1
-        if j == n:
-            j = 0
-        pos = [i, j]
-        ax.plot(x[pos], y[pos], lw=3, color="black")
-
-
-im = Image.open("Imagen.tif")
-FFT_image = FFT(im)
-plot_FFT_and_original(im, FFT_image, name="FFT")
+inputs = {
+    "Image path": "",
+    "Image name": "Imagen.tif",
+}
+FFT = FFT_algorithm(inputs["Image path"],
+                    inputs["Image name"])
+FFT.plot_FFT_and_original()
